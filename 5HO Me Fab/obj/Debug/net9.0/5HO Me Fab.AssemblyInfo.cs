85c0110d68c570edb528d8e338f85a2e2e90071d
Drennon <dk1008065@otc.edu>
2026-03-06 22:06:47 -0600

    Updated to Visual Studio 2026

--- 5HO Me Fab/obj/Debug/net9.0/5HO Me Fab.AssemblyInfo.cs
+++ 5HO Me Fab/obj/Debug/net9.0/5HO Me Fab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5HO Me Fab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a931e001ab3f2c636596ca5a165f33739355c0d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785c1cd475af1d65f62b55a1bcb2a17e108cd944")]
 [assembly: System.Reflection.AssemblyProductAttribute("5HO Me Fab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5HO Me Fab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
