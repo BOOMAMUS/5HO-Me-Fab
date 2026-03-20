9fcc2425bd801570510e07cf4a40a444795d5bc6
Drennon <dk1008065@otc.edu>
2026-03-20 17:42:16 -0500

    Populated Dropdowns

---
+++ 5HO Me Fab/Views/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _5HO_Me_Fab.Views
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
