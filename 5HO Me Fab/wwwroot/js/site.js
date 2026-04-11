d03170efb0b22b69c4009cb970a7dd03ce7d672b
Drennon <dk1008065@otc.edu>
2026-04-10 22:51:56 -0500

    Added customizations and descriptions back. Added a functional Tank.png onto the index. Exports customizations to xlsx Excel file.

--- 5HO Me Fab/wwwroot/js/site.js
+++ 5HO Me Fab/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Need to handle the exception thrown here when swtiching pages.
+// Null reference when switching pages unhandled.
+// JavaScript debugging necessary to see all the descriptions on the index page.
+
+const tankDescriptions = window.tankLookupData;
+
+function updateDesc(selectId, descId) {
+    const selected = document.getElementById(selectId).value;
+    document.getElementById(descId).innerText = tankDescriptions[selected] || "";
+}
+
+function bindDesc(selectId, descId) {
+    const el = document.getElementById(selectId);
+    if (el) {
+        el.addEventListener("change", () => updateDesc(selectId, descId));
+    }
+}
+
+bindDesc("TankCust1", "desc1");
+bindDesc("TankCust2", "desc2");
+bindDesc("TankCust3", "desc3");
+bindDesc("TankCust4", "desc4");
+bindDesc("TankCust5", "desc5");
+bindDesc("TankCust6", "desc6");
